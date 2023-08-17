65c03d3cba0b6f3b5e5cf8442476b771a950e719
Wilson <wj.buena@gmail.com>
2023-08-17 10:00:48 +0800

    Add typeDefs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
 
+// types
+import { typeDefs } from './schema'
+
 // Server Setup
 const server = new ApolloServer({
     // typeDefs
+    typeDefs,
     // resolvers
 })
 
