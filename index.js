8c3027b72580e0a1628c19dd48e99b697592c402
Wilson <wj.buena@gmail.com>
2023-08-18 10:09:30 +0800

    Resolver functions

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 import { ApolloServer } from '@apollo/server'
 import { startStandaloneServer } from '@apollo/server/standalone'
+import db from './_db.js'
 
 // types
-import { typeDefs } from './schema'
+import { typeDefs } from './schema.js'
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        }
+    }
+}
 
 // Server Setup
 const server = new ApolloServer({
     // typeDefs
     typeDefs,
     // resolvers
+    resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
